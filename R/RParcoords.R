259daae67da6338e82778399fb8f2daba5d315b3
Bertjan Broeksema <broeksem@lippmann.lu>
2014-08-20 17:19:59 +0200

    Add a function to filter by extent.

--- R/RParcoords.R
+++ R/RParcoords.R
@@ -9,3 +9,36 @@
 #
 # > /ocpu/library/RParcoords/data/cstr.my/json?auto_unbox=true
 
+
+#' Hello World
+#'
+#' Basic hello world function to be called from the demo app
+#'
+#' @export
+#' @param data - The data to be filtered. Will fall back to cstr, if not
+#'               specified.
+#' @param extents - Named list with one or more extents selecting ranges of
+#'                  variables. The extents are inclusive, i.e. values equal to
+#'                  min and values equal to max, are considered to fall wihtin
+#'                  the extents.
+#' @param method - One of: (KEEP|REMOVE). In the KEEP case, all observations
+#'                 that fall within the extents are being kept. In the REMOVE
+#'                 case, only observations that fall outside the extends are
+#'                 being kept.
+filterByExtents <- function(data = cstr[["data"]], extents, method = "KEEP") {
+  filtered <- data
+
+  methods <- list(
+    "KEEP" = function(name, extents) {
+      select <- filtered[name] >= extents[1] & filtered[name] <= extents[2]
+      filtered <<- filtered[select, ]
+    },
+    "REMOVE" = function(name, extents) {
+      select <- filtered[name] < extents[1] | filtered[name] > extents[2]
+      filtered <<- filtered[select, ]
+    }
+  )
+
+  Map(methods[[method]], names(extents), extents)
+  filtered
+}
