6963a6ef8dba08d9720f95be68295f4d8109f0d9
Bertjan Broeksema <broeksem@lippmann.lu>
2014-08-28 17:03:55 +0200

    Optimize filtering a bit.
    Update a boolean vector which represents the selection and only do the
    selection once, at the end.

--- R/data.R
+++ R/data.R
@@ -10,19 +10,21 @@ data.get <- function(data = cstr.all, variables) {
 }
 
 data.filter <- function(data = cstr.all, extents, method = "KEEP") {
-  filtered <- data
+  finalSelection <- as.logical(c(1:nrow(data)))
 
   methods <- list(
     "KEEP" = function(name, extents) {
       select <- filtered[name] >= extents[1] & filtered[name] <= extents[2]
-      filtered <<- filtered[select, ]
+      finalSelection <<- finalSelection & select
     },
     "REMOVE" = function(name, extents) {
       select <- filtered[name] < extents[1] | filtered[name] > extents[2]
-      filtered <<- filtered[select, ]
+      finalSelection <<- finalSelection & select
     }
   )
 
   Map(methods[[method]], names(extents), extents)
-  filtered
+
+  filtered <- data
+  filtered[finalSelection, ]
 }
