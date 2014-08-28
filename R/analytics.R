6b6b3e70d9071971ed451a042817bbc8a14f3913
Bertjan Broeksema <broeksem@lippmann.lu>
2014-08-28 16:21:21 +0200

    Add first clustering code in the backend.

---
+++ R/analytics.R
@@ -0,0 +1,16 @@
+
+cluster.kmeans <- function(data, vars, centers = NA, iter.max=10) {
+  # All numeric cols in the data frame
+  clusterVars <- names(data)[sapply(data, is.numeric)]
+  clusterVars <- Filter(function(x) { x %in% vars }, clusterVars)
+  data <- data[ ,clusterCols]
+  stats::kmeans(data, centers, iter.max)$cluster
+}
+
+cluster.methods <- function() {
+  list("kmeans" = list(
+    "vars" = list("type"="schema.numeric", "required"=T),
+    "centers" = list("type"="numeric", "required"=T),
+    "iter.max" = list("type"="numeric", "required"=F, "default"=10)
+  ))
+}
