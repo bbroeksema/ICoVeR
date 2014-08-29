500f88dc84d192a1450f25c422874f7ad595510f
Bertjan Broeksema <broeksem@lippmann.lu>
2014-08-29 15:08:11 +0200

    Basic support in the front-end for clustering

--- inst/www/js/controllers/clusterctrl.js
+++ inst/www/js/controllers/clusterctrl.js
@@ -1,9 +1,82 @@
 'use strict';
 
 angular.module('contigBinningApp.controllers')
-  .controller('ClusterCtrl', function ($scope, DataSet) {
+  .controller('ClusterCtrl', function ($scope, $http, $modal, DataSet, Analytics, R) {
+    var d = {
+      schema: undefined
+    };
+
+    function setVariables() {
+      if ($scope.selectedClusterMethod === undefined || d.schema === undefined) {
+        $scope.variables = [];
+      } else {
+        $scope.variables = _.filter(d.schema, function(variable) {
+          return R.is.numeric(variable.type)
+        });
+        $scope.selectedVariables = [];
+      }
+    };
+
+    function updateSelectedVariables(variables) {
+      var text = _.reduce(variables, function(str, variable) {
+        return str === "" ? variable.name : str + ", " + variable.name;
+      }, "");
+      if (text.length > "Select variables...".length) {
+        $scope.selectionTextLong = text;
+        text = "Selected " + variables.length + " variables";
+      }
+      $scope.selectionText = text;
+      $scope.selectedVariables = variables;
+      $scope.configurationInvalid = $scope.selectedVariables.length === 0;
+    }
+
+    $scope.selectionText = "Select variables...";
+    $scope.selectionTextLong = "No variables selected";
+    $scope.variables = [];
+    $scope.clusterMethods = [];
 
     $scope.configurationInvalid = true;
+    $scope.selectedClusterMethod = $scope.clusterMethods[0];
+    $scope.selectedVariables = [];
 
-    $scope.$on('DataSet::loaded', function() { $scope.dataAvailable = true });
+    $scope.$watch('selectedClusterMethod', function(newMethod, oldMethod) {
+      if (newMethod === undefined) return;
+    });
+
+    $scope.$on('DataSet::schemaLoaded', function(e, schema) {
+      $scope.dataAvailable = true
+      d.schema = schema;
+      setVariables();
+    });
+
+    $scope.$on('Analytics::clusterMethodsAvailable', function(e, methods) {
+      $scope.clusterMethods = methods;
+      $scope.selectedClusterMethod = $scope.clusterMethods[0];
+      setVariables();
+    });
+
+    $scope.openSelectionDialog = function() {
+      var dialog = $modal.open({
+        templateUrl: 'js/templates/selectvars.html',
+        size: 'sm',
+        controller: 'VariableSelectionCtrl',
+        resolve: {
+          variables: function() {
+            return $scope.variables;
+          },
+          selected: function() {
+            return $scope.selectedVariables;
+          }
+        }
+      });
+
+      dialog.result.then(updateSelectedVariables);
+    };
+
+    $scope.cluster = function() {
+      Analytics.cluster(
+        $scope.selectedClusterMethod.name,
+        _.map($scope.selectedVariables, function(variable) { return variable.name; }),
+        null);
+    }
   });
---
+++ inst/www/js/controllers/variableselectionctrl.js
@@ -0,0 +1,18 @@
+angular.module('contigBinningApp.controllers')
+  .controller('VariableSelectionCtrl', function($scope, $modalInstance, variables, selected) {
+
+    $scope.variables = variables;
+    // https://stackoverflow.com
+    //   /questions/21379173/angularjs-ui-modal-and-select-doesnt-update-the-scope-values
+    $scope.selected = {
+      variables: selected
+    }
+
+    $scope.ok = function() {
+      $modalInstance.close($scope.selected.variables);
+    };
+
+    $scope.cancel = function() {
+      $modalInstance.dismiss();
+    };
+  })
