19889bb195212667f5311fb1f74d3862c7f887ff
Bertjan Broeksema <broeksem@lippmann.lu>
2014-08-29 15:06:55 +0200

    Get rid of the multiselect dependency.

--- inst/www/js/app.js
+++ inst/www/js/app.js
@@ -9,9 +9,3 @@ angular.module('contigBinningApp', [
   'contigBinningApp.controllers',
   'contigBinningApp.services',
 ]);
-
-$(document).ready(function() {
-    $('.multiselect').multiselect({
-        buttonClass: 'btn btn-default btn-sm'
-    });
-});
