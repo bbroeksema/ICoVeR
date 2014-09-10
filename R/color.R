2d93500bbadf2e01d96608355cf3d19056e1938b
Bertjan Broeksema <broeksem@lippmann.lu>
2014-09-10 13:55:43 +0200

    Add first backend code for coloring.

---
+++ R/color.R
@@ -0,0 +1,79 @@
+p_schemes <- list(
+  numeric = list(
+    value = list(
+      blue_to_brown = c("steelblue", "brown")
+    )
+  )
+)
+
+p_color_numeric_value <- function(colorVariable, scheme) {
+  # First we extract the variable and normalize it.
+  extent <- range(colorVariable)
+  normalized <- colorVariable - extent[1]
+  normalized <- normalized / (extent[2] - extent[1])
+
+  # Now we create a color interpolation function and call it on the normalized
+  # data.
+  color <- colorRamp(p_schemes$numeric$value[[scheme]])
+  rgb(color(normalized), max = 255)
+}
+
+p_color_numeric_decile <- function(colorVariable, scheme) {
+
+}
+
+p_color_numeric_zscore <- function(colorVariable, scheme) {
+
+}
+
+p_color_numeric <- function(colorVariable, method, scheme) {
+  if (method == "Value") {
+    p_color_numeric_value(colorVariable, scheme)
+  } else if (method == "Decile") {
+    p_color_numeric_decile(colorVariable, scheme)
+  } else if (method == "Z-score") {
+    p_color_numeric_zscore(colorVariable, scheme)
+  } else {
+    stop(paste("Unsupported color method:", method))
+  }
+}
+
+color.configurations <- function() {
+  list(
+    numeric = list(
+      "Value" = names(p_schemes$numeric$value),
+      "Decile" = c("BlueToGreen", "BlueToPurple", "Blues"),
+      "Z-score" = c("RedToBlue", "Spectral")
+    )
+  )
+}
+
+# color.apply(variable="gc_content", method="Value", scheme="blue_to_brown")
+color.apply <- function(rows = c(), variable, method, scheme) {
+  colorVariable <- data.get(rows, c(variable))
+
+  if (is.numeric(colorVariable)) {
+    p_color_numeric(colorVariable, method, scheme)
+  } else {
+    stop("Unsupported data type")
+  }
+
+  # Now, combine row names and the colors in a resulting data frame.
+  #data.frame("row" = rows, "vis.color" = colors, stringsAsFactors = F)
+}
+
+color.get <- function(colors = NA, rows = c()) {
+  if (missing(colors)) {
+    if (length(rows) == 0) {
+      rep("#069", nrow(gData))
+    } else {
+      rep("#069", length(rows))
+    }
+  } else {
+    if (length(rows) == 0 ) {
+      colors;
+    } else {
+      colors[rows]
+    }
+  }
+}
