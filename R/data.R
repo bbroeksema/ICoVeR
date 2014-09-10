6146126b70c7dda9b719b2da690648e8d9879ddc
Bertjan Broeksema <broeksem@lippmann.lu>
2014-09-10 13:57:33 +0200

    Make the data.* of the backend row based.
    That is, in stead of returning the full data.frame upon filtering, we
    return the row numbers. Those are always row numbers in the full dataset.

--- R/data.R
+++ R/data.R
@@ -1,28 +1,44 @@
 data(cstr.schema)
 data(cstr)
 
+gSchema <- cstr.schema;
+gData <- cstr;
+
 data.schema <- function() {
-  cstr.schema
+  gSchema
 }
 
-data.get <- function(data = cstr, variables) {
-  data[ ,variables]
+# data.get(rows = c(1,2,3,5,10), variables = c("gc_content"))
+# data.get(variables = c("gc_content"))
+data.get <- function(rows = NA, variables) {
+  if (missing(rows) | length(rows) == 0)
+    gData[ ,variables]
+  else
+    gData[rows, variables]
 }
 
-data.filter <- function(data = cstr, extents, method = "KEEP") {
-  finalSelection <- as.logical(c(1:nrow(data)))
+data.filter <- function(rows = c(), extents, method = "KEEP") {
+  initialData <- NA
+  if (missing(rows)) {
+    initialData <- gData
+  } else {
+    initialData <- gData[rows, ]
+  }
+
+  rows <- ifelse(missing(rows), nrow(gData), length(rows))
+  finalSelection <- as.logical(c(1:rows))
 
   methods <- list(
     "KEEP" = function(name, extents) {
-      select <- data[name] >= extents[1] & data[name] <= extents[2]
+      select <- initialData[name] >= extents[1] & initialData[name] <= extents[2]
       finalSelection <<- finalSelection & select
     },
     "REMOVE" = function(name, extents) {
-      select <- data[name] < extents[1] | data[name] > extents[2]
+      select <- initialData[name] < extents[1] | initialData[name] > extents[2]
       finalSelection <<- finalSelection & select
     }
   )
 
   Map(methods[[method]], names(extents), extents)
-  data[finalSelection, ]
+  attr(initialData[finalSelection, ], "row.names")
 }
