218f0aea31680dc92eda02913dd8367092dc40e2
Bertjan Broeksema <broeksem@lippmann.lu>
2014-09-11 15:46:42 +0200

    Implement theme switching.
    This still needs some refinement, as the parcoords is not updated correctly
    yet.

---
+++ inst/www/js/controllers/themectrl.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('contigBinningApp.controllers')
+  .controller('ThemeCtrl', function($scope) {
+    
+    var link = $('#maincss');
+    
+    $scope.theme = 'light';
+    $scope.$watch('theme', function(newTheme) {
+       var url = 'css/' + newTheme + '.css';
+       if (link.attr('href') !== url) {
+         link.attr('href', url);
+       }
+    });
+  });
