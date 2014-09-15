7fa5eb5d489057e54c3fe971bf2e5d3cdbf3673f
Bertjan Broeksema <broeksem@lippmann.lu>
2014-09-15 14:19:35 +0200

    Fix some bugs with the indicator.

--- inst/www/js/controllers/dataindicatorctrl.js
+++ inst/www/js/controllers/dataindicatorctrl.js
@@ -4,24 +4,25 @@ angular.module('contigBinningApp.controllers')
   .controller('DataIndicatorCtrl', function ($scope) {
       
       $scope.rowCount = 0;
+      $scope.currentRowCount = 0;
       $scope.counts = [];
       
       $scope.$on("DataSet::dataLoaded", function(ev, data) {
         if ($scope.rowCount === 0) {
           $scope.rowCount = data.length;
         }
-        $scope.counts = [{ rows: $scope.rowCount, type: "" }];
+        $scope.currentRowCount = data.length;
+        $scope.counts = [{ rows: $scope.currentRowCount, type: "" }];
       });
       
       $scope.$on("DataSet::brushed", function(ev, extents, rows) {
         if (rows) {
           $scope.counts = [
             { rows: rows.length, type: "brushed" },
-            { rows: $scope.rowCount - rows.length, type: "" }
+            { rows: $scope.currentRowCount - rows.length, type: "" }
           ];
         } else {
-          $scope.counts = [{ rows: $scope.rowCount, type: "" }];
+          $scope.counts = [{ rows: $scope.currentRowCount, type: "" }];
         }
-        console.log($scope.counts);
       });
   });
