a37fdb359f982415c110d30ece7f6d94de4f5162
Bertjan Broeksema <broeksem@lippmann.lu>
2014-09-16 14:51:57 +0200

    Add a service for Parcoords configuration.
    This is for sharing configuration between the parcoords visualization and
    the various control widgets that will soon appear to modify the appearance
    of the parcoords.

---
+++ inst/www/js/services/parcoords.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// This service is meant to share state betweem the parcoords visualization and
+// the various control widgets
+
+angular.module('contigBinningApp.services')
+  .service('ParCoords', function($rootScope, R) {
+    
+    var d = {
+      variables: [],         // List of variables that can be displayed in parcoords.
+      selectedVariables: [], // List of variables currently displayed in parcoords.
+      
+      selectionText: "",
+      selectionTextLong: ""
+    };
+    
+    function setVariables(schema) {
+      if (schema === undefined) {
+        d.variables = [];
+      } else {
+        d.variables = _.filter(schema, function(variable) {
+          return R.is.numeric(variable.type);
+        });
+        d.selectedVariables = [];
+      }
+    }
+    
+    d.updateSelectedVariables = function(variables) {
+      var text = _.reduce(variables, function(str, variable) {
+        return str === "" ? variable.name : str + ", " + variable.name;
+      }, "");
+      
+      if (text.length > "Select variables...".length) {
+        d.selectionTextLong = text;
+        text = "Selected " + variables.length + " variables";
+      }
+
+      d.selectionText = text;
+      d.selectedVariables = variables;
+      $rootScope.$broadcast("ParCoords::selectedVariablesChanged");
+    }
+    
+    $rootScope.$on('DataSet::schemaLoaded', function(e, schema) {
+      setVariables(schema);
+      
+      var variables = _.filter(d.variables, function(variable) {
+        return R.is.numeric(variable["type"]) &&
+          (variable["group.type"] === "Characteristics"
+          || variable["group.type"] === "TimeSeries")
+      });
+      d.updateSelectedVariables(variables);
+    });
+   
+    return d;
+  });
