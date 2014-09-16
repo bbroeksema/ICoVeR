ea5f7f06e8d6d9e78bb35e1cbbcf7a5faea5da15
Bertjan Broeksema <broeksem@lippmann.lu>
2014-09-16 17:53:56 +0200

    Add. R.is.factor(). We'll need this for dealing with cluster results.

--- inst/www/js/services/r.js
+++ inst/www/js/services/r.js
@@ -4,6 +4,9 @@ angular.module('contigBinningApp.services')
   .service('R', function($rootScope, $http) {
     return {
       is: {
+        factor: function(typestr) {
+          return typestr === "factor";
+        },
         numeric: function(typestr) {
           return typestr === "numeric" || typestr === "integer";
         }
