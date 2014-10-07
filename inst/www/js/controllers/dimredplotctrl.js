45093798a56830da8a2eb576549027130399f840
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-07 16:03:28 +0200

    Put in the new DimRedPlot component

---
+++ inst/www/js/controllers/dimredplotctrl.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('contigBinningApp.controllers')
+  .controller('DimRedPlotCtrl', function ($scope, $element, $window, OpenCPU) {
+
+    var plot = crpgl.DimRedPlot();
+    
+    function resize() {
+      plot
+        .width($element[0].clientWidth)
+        .height($element[0].clientHeight);
+      d3.select($element[0]).call(plot);
+    }
+    
+    function updatePlot(session, json) {
+      console.log(json);
+      d3.select($element[0])
+        .datum(json.plotdata)
+        .call(plot);
+    }
+
+    angular.element($window).bind('resize', resize);
+    $(document).ready(resize);
+
+    // Draw an empty plot at first.
+    d3.json("libs/dimredplot/ca.json", updatePlot);
+    
+    $scope.$on("Analytics::dimensionalityReduced", function(ev, method, session) {
+      var fnName = "dimred." + method + ".plotdata",
+          args = { ca: session };
+      
+      OpenCPU.json(fnName, args, updatePlot)
+    })
+  });
