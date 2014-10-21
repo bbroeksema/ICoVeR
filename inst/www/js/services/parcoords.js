54ad5a2abeedff252a94622f3c0e25d95bf30f90
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 09:12:25 +0200

    Fix jslint issues in services/parcoords.js

--- inst/www/js/services/parcoords.js
+++ inst/www/js/services/parcoords.js
@@ -1,39 +1,42 @@
+/*jslint white: false, indent: 2, nomen: true */
+/*global angular, _ */
+
 'use strict';
 
 // This service is meant to share state betweem the parcoords visualization and
 // the various control widgets
 
 angular.module('contigBinningApp.services')
-  .service('ParCoords', function($rootScope, R) {
-    
+  .service('ParCoords', function ($rootScope, R) {
     var d = {
-      variables: [],         // List of variables that can be displayed in parcoords.
-      selectedVariables: [], // List of variables currently displayed in parcoords.
-      
-      selectionText: "",
-      selectionTextLong: "",
+        variables: [],         // List of variables that can be displayed in parcoords.
+        selectedVariables: [], // List of variables currently displayed in parcoords.
 
-      brushPredicates: ["AND", "OR"],
-      brushPredicate: undefined
-    };
+        selectionText: "",
+        selectionTextLong: "",
+
+        brushPredicates: ["AND", "OR"],
+        brushPredicate: undefined,
+        highlightFunction: undefined
+      };
 
     function setVariables(schema) {
       if (schema === undefined) {
         d.variables = [];
       } else {
-        d.variables = _.filter(schema, function(variable) {
+        d.variables = _.filter(schema, function (variable) {
           return R.is.numeric(variable.type) ||
             R.is.factor(variable.type);
         });
         d.selectedVariables = [];
       }
     }
-    
-    d.updateSelectedVariables = function(variables) {
-      var text = _.reduce(variables, function(str, variable) {
+
+    d.updateSelectedVariables = function (variables) {
+      var text = _.reduce(variables, function (str, variable) {
         return str === "" ? variable.name : str + ", " + variable.name;
       }, "");
-      
+
       if (text.length > "Select variables...".length) {
         d.selectionTextLong = text;
         text = "Selected " + variables.length + " variables";
@@ -42,36 +45,37 @@ angular.module('contigBinningApp.services')
       d.selectionText = text;
       d.selectedVariables = variables;
       $rootScope.$broadcast("ParCoords::selectedVariablesChanged");
-    }
+    };
 
-    d.updateBrushPredicate = function(newPredicate) {
+    d.updateBrushPredicate = function (newPredicate) {
       if (d.brushPredicate !== newPredicate) {
         d.brushPredicate = newPredicate;
         $rootScope.$broadcast("ParCoords::brushPredicateChanged", d.brushPredicate);
       }
     };
 
-    $rootScope.$on('DataSet::schemaLoaded', function(e, schema) {
+    /*jslint unparam: true */
+    $rootScope.$on('DataSet::schemaLoaded', function (e, schema) {
       setVariables(schema);
-      
-      var variables = _.filter(d.variables, function(variable) {
-        return R.is.numeric(variable["type"]) &&
+
+      var variables = _.filter(d.variables, function (variable) {
+        return R.is.numeric(variable.type) &&
           (variable["group.type"] === "Characteristics"
-          || variable["group.type"] === "TimeSeries")
+          || variable["group.type"] === "TimeSeries");
       });
       d.updateSelectedVariables(variables);
     });
-    
-    var highlightFunction = null;
-    d.setHighlightFunction = function(highlightFunc) {
-      highlightFunction = highlightFunc;
-    }
+    /*jslint unparam: false */
 
-    d.highlightRow = function(rowIndex) {
-      if(highlightFunction) {
-        highlightFunction(rowIndex);
+    d.setHighlightFunction = function (highlightFunc) {
+      d.highlightFunction = highlightFunc;
+    };
+
+    d.highlightRow = function (rowIndex) {
+      if (d.highlightFunction) {
+        d.highlightFunction(rowIndex);
       }
-    }
+    };
 
     return d;
   });
