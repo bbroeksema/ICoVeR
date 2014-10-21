2e9124937b907da0d722e31ceaa4972563cd97d7
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 11:31:25 +0200

    Fix jslint issues for js/controllers/filterctrl.js

--- inst/www/js/controllers/filterctrl.js
+++ inst/www/js/controllers/filterctrl.js
@@ -1,32 +1,41 @@
+/*jslint indent: 2 */
+/*global angular */
+
 'use strict';
 
 angular.module('contigBinningApp.controllers')
-  .controller('FilterCtrl', function ($scope, $rootScope, DataSet) {
+  .controller('FilterCtrl', function ($scope, DataSet) {
 
     $scope.itemsBrushed = false;
     $scope.dataFiltered = false;
     $scope.filteringInProgress = false;
 
-    $scope.keepSelected = function() {
+    $scope.keepSelected = function () {
       $scope.filteringInProgress = true;
       DataSet.filter(DataSet.FilterMethod.KEEP);
     };
 
-    $scope.removeSelected = function() {
+    $scope.removeSelected = function () {
       $scope.filteringInProgress = true;
       DataSet.filter(DataSet.FilterMethod.REMOVE);
     };
 
-    $scope.reloadData = function() {
+    $scope.reloadData = function () {
       DataSet.filter(DataSet.FilterMethod.RESET);
-    }
+    };
 
-    $scope.$on('DataSet::loaded', function() { $scope.dataFiltered = false });
-    $scope.$on('DataSet::filtered', function(e, method) {
+    $scope.$on('DataSet::loaded', function () { $scope.dataFiltered = false; });
+
+    /*jslint unparam: true */
+    $scope.$on('DataSet::filtered', function (e, method) {
       $scope.filteringInProgress = false;
       $scope.dataFiltered = method !== DataSet.FilterMethod.RESET;
     });
-    $scope.$on('DataSet::brushed', function(e, extents) {
+    /*jslint unparam: false */
+
+    /*jslint unparam: true */
+    $scope.$on('DataSet::brushed', function (e, extents) {
       $scope.itemsBrushed = Object.getOwnPropertyNames(extents).length > 0;
     });
+    /*jslint unparam: false */
   });
