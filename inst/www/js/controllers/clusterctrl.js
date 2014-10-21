f2d6bc06cb85c1d56f0efc4a5102bf454956461f
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 11:09:42 +0200

    Fix jslint issues in js/controllers/clusterctrl.js

--- inst/www/js/controllers/clusterctrl.js
+++ inst/www/js/controllers/clusterctrl.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('contigBinningApp.controllers')
-  .controller('ClusterCtrl', function ($scope, $http, $modal, DataSet, Analytics, R) {
+  .controller('ClusterCtrl', function ($scope, $modal, Analytics, R) {
     var d = {
       schema: undefined
     };
