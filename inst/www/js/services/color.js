4b6bd4a3a26dbf8de8eb5e10b1ee45997c680d8d
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 10:29:29 +0200

    Fix jslint issues in js/services/color.js

--- inst/www/js/services/color.js
+++ inst/www/js/services/color.js
@@ -1,26 +1,34 @@
+/*jslint indent: 2, white: false */
+/*global angular */
+
 'use strict';
 
 angular.module('contigBinningApp.services')
-  .service('Color', function($rootScope, R, DataSet, OpenCPU) {
+  .service('Color', function ($rootScope, DataSet, OpenCPU) {
 
-    OpenCPU.json("color.configurations", {}, function(session, config) {
+    /*jslint unparam: true */
+    OpenCPU.json("color.configurations", {}, function (session, config) {
       $rootScope.$broadcast("Colors::configurationLoaded", config);
     });
-    
+    /*jslint unparam: false */
+
     return {
-      color: function(variable, method, scheme) {        
+      color: function (variable, method, scheme) {
         var args = {
           variable: variable,
           method: method,
           scheme: scheme
-        }
+        };
+
         if (DataSet.rows()) {
           args.rows = DataSet.rows();
         }
-        
-        OpenCPU.json("color.apply", args, function(session, colors) {
+
+        /*jslint unparam: true */
+        OpenCPU.json("color.apply", args, function (session, colors) {
           $rootScope.$broadcast("Colors::changed", colors);
         });
+        /*jslint unparam: false */
       }
-    }
+    };
   });
