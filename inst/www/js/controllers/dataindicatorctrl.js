5852a89279a6b262a2e06e59e07e3041d713cebd
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 10:57:27 +0200

    Fix jslint issues for js/controllers/dataindicatorctrl.js

--- inst/www/js/controllers/dataindicatorctrl.js
+++ inst/www/js/controllers/dataindicatorctrl.js
@@ -1,31 +1,36 @@
+/*jslint indent: 2 */
+/*global angular */
+
 'use strict';
 
 angular.module('contigBinningApp.controllers')
-  .controller('DataIndicatorCtrl', function ($scope,DataSet) {
-      
-      $scope.rowCount = 0;
-      $scope.currentRowCount = 0;
-      $scope.counts = [];
+  .controller('DataIndicatorCtrl', function ($scope, DataSet) {
 
-      $scope.$on("DataSet::brushed", function(ev, extents, rows) {
-        if (Object.getOwnPropertyNames(extents).length > 0) {
-          $scope.counts = [
-            { rows: rows.length, type: "brushed" },
-            { rows: $scope.currentRowCount - rows.length, type: "" }
-          ];
-        } else {
-          $scope.counts = [{ rows: $scope.currentRowCount, type: "" }];
-        }
-      });
+    $scope.rowCount = 0;
+    $scope.currentRowCount = 0;
+    $scope.counts = [];
 
-     // setting the call back  for the total row count
-      DataSet.getTotalNumRows(function (rowCount) {
-        $scope.rowCount  = rowCount;
-      });
-
-       // setting the call back  for the current row count
-      DataSet.setCurrentNumRowsCallback(function(currentRowCount) {
-        $scope.currentRowCount = currentRowCount;
+    /*jslint unparam: true */
+    $scope.$on("DataSet::brushed", function (ev, extents, rows) {
+      if (Object.getOwnPropertyNames(extents).length > 0) {
+        $scope.counts = [
+          { rows: rows.length, type: "brushed" },
+          { rows: $scope.currentRowCount - rows.length, type: "" }
+        ];
+      } else {
         $scope.counts = [{ rows: $scope.currentRowCount, type: "" }];
-      });
+      }
+    });
+    /*jslint unparam: false */
+
+    // setting the call back  for the total row count
+    DataSet.getTotalNumRows(function (rowCount) {
+      $scope.rowCount  = rowCount;
+    });
+
+    // setting the call back  for the current row count
+    DataSet.setCurrentNumRowsCallback(function (currentRowCount) {
+      $scope.currentRowCount = currentRowCount;
+      $scope.counts = [{ rows: $scope.currentRowCount, type: "" }];
+    });
   });
