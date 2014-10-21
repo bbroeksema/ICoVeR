32a4c46b451202f6c34c7e5c3f18a1319e8e806d
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 10:47:57 +0200

    Fix jslint issues for js/controllers/clusterctrl.js

--- inst/www/js/controllers/clusterctrl.js
+++ inst/www/js/controllers/clusterctrl.js
@@ -1,3 +1,6 @@
+/*jslint indent: 2, nomen: true */
+/*global angular, _ */
+
 'use strict';
 
 angular.module('contigBinningApp.controllers')
@@ -10,15 +13,15 @@ angular.module('contigBinningApp.controllers')
       if ($scope.selectedClusterMethod === undefined || d.schema === undefined) {
         $scope.variables = [];
       } else {
-        $scope.variables = _.filter(d.schema, function(variable) {
-          return R.is.numeric(variable.type)
+        $scope.variables = _.filter(d.schema, function (variable) {
+          return R.is.numeric(variable.type);
         });
         $scope.selectedVariables = [];
       }
-    };
+    }
 
     function updateSelectedVariables(variables) {
-      var text = _.reduce(variables, function(str, variable) {
+      var text = _.reduce(variables, function (str, variable) {
         return str === "" ? variable.name : str + ", " + variable.name;
       }, "");
       if (text.length > "Select variables...".length) {
@@ -39,32 +42,38 @@ angular.module('contigBinningApp.controllers')
     $scope.selectedClusterMethod = $scope.clusterMethods[0];
     $scope.selectedVariables = [];
 
-    $scope.$watch('selectedClusterMethod', function(newMethod, oldMethod) {
-      if (newMethod === undefined) return;
+    /*jslint unparam: true */
+    $scope.$watch('selectedClusterMethod', function (newMethod, oldMethod) {
+      if (newMethod === undefined) { return; }
     });
+    /*jslint unparam: false */
 
-    $scope.$on('DataSet::schemaLoaded', function(e, schema) {
-      $scope.dataAvailable = true
+    /*jslint unparam: true */
+    $scope.$on('DataSet::schemaLoaded', function (e, schema) {
+      $scope.dataAvailable = true;
       d.schema = schema;
       setVariables();
     });
+    /*jslint unparam: false */
 
-    $scope.$on('Analytics::clusterMethodsAvailable', function(e, methods) {
+    /*jslint unparam: true */
+    $scope.$on('Analytics::clusterMethodsAvailable', function (e, methods) {
       $scope.clusterMethods = methods;
       $scope.selectedClusterMethod = $scope.clusterMethods[0];
       setVariables();
     });
+    /*jslint unparam: false */
 
-    $scope.openSelectionDialog = function() {
+    $scope.openSelectionDialog = function () {
       var dialog = $modal.open({
         templateUrl: 'js/templates/selectvars.html',
         size: 'sm',
         controller: 'VariableSelectionCtrl',
         resolve: {
-          variables: function() {
+          variables: function () {
             return $scope.variables;
           },
-          selected: function() {
+          selected: function () {
             return $scope.selectedVariables;
           }
         }
@@ -73,10 +82,12 @@ angular.module('contigBinningApp.controllers')
       dialog.result.then(updateSelectedVariables);
     };
 
-    $scope.cluster = function() {
+    $scope.cluster = function () {
       Analytics.cluster(
         $scope.selectedClusterMethod.name,
-        _.map($scope.selectedVariables, function(variable) { return variable.name; }),
-        null);
-    }
+        _.map($scope.selectedVariables,
+              function (variable) { return variable.name; }),
+        null
+      );
+    };
   });
