8b417c345bea2eed8c820d617cd3ba0df136a737
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 15:44:52 +0200

    Fix jslint issues in js/controllers/namehighlightcontroller.js

--- inst/www/js/controllers/namehighlightctrl.js
+++ inst/www/js/controllers/namehighlightctrl.js
@@ -1,37 +1,42 @@
+/*jslint indent: 2 */
+/*global angular */
+
 'use strict';
 
 angular.module('contigBinningApp.controllers')
-  .controller('NameHighlightCtrl', function ($scope,DataSet,ParCoords) {
-    var nameHash = {};
-    var variables = [];
+  .controller('NameHighlightCtrl', function ($scope, DataSet, ParCoords) {
+    var nameHash = {},
+      variables = [];
 
-    function buildNameHash(data){
+    function buildNameHash(data) {
       nameHash = {};
-      data.forEach(function(d,dataIndex) {
-        variables.forEach(function(v){
+      data.forEach(function (d, dataIndex) {
+        variables.forEach(function (v) {
           nameHash[d[v]] = dataIndex;
         });
       });
     }
 
-    $scope.$on('DataSet::schemaLoaded', function(e, schema) {
-     variables = [];
-     console.log("Schema Recieved");
-     schema.forEach(function(d){
-       if(d.group ==="Id") {
-         variables.push(d.name);
-       }
-     });
-      DataSet.get(variables,buildNameHash);
+    /*jslint unparam: true */
+    $scope.$on('DataSet::schemaLoaded', function (e, schema) {
+      variables = [];
+      schema.forEach(function (d) {
+        if (d.group === "Id") {
+          variables.push(d.name);
+        }
+      });
+      DataSet.get(variables, buildNameHash);
     });
+    /*jslint unparam: false */
 
-   $scope.highlightRow = function() {
-     // called by ng_change directive on the rowName input in the UI
-     var highlightIndex = -1;
-     if(nameHash.hasOwnProperty($scope.rowName)) {
-       highlightIndex = nameHash[$scope.rowName];
-     } 
-     ParCoords.highlightRow(highlightIndex);
-   }
+    $scope.highlightRow = function () {
+      // called by ng_change directive on the rowName input in the UI
+      var highlightIndex = -1;
+      if (nameHash.hasOwnProperty($scope.rowName)) {
+        highlightIndex = nameHash[$scope.rowName];
+      }
+      ParCoords.highlightRow(highlightIndex);
+    };
 
   });
+
