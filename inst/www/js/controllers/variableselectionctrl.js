1b93b8bb631b693ce69395b304e212bb676d90ce
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 13:20:58 +0200

    Fix jslint issues in js/controllers/variableselectionctrl.js

--- inst/www/js/controllers/variableselectionctrl.js
+++ inst/www/js/controllers/variableselectionctrl.js
@@ -1,18 +1,23 @@
+/*jslint indent: 2 */
+/*global angular */
+
+'use strict';
+
 angular.module('contigBinningApp.controllers')
-  .controller('VariableSelectionCtrl', function($scope, $modalInstance, variables, selected) {
+  .controller('VariableSelectionCtrl', function ($scope, $modalInstance, variables, selected) {
 
     $scope.variables = variables;
     // https://stackoverflow.com
     //   /questions/21379173/angularjs-ui-modal-and-select-doesnt-update-the-scope-values
     $scope.selected = {
       variables: selected
-    }
+    };
 
-    $scope.ok = function() {
+    $scope.ok = function () {
       $modalInstance.close($scope.selected.variables);
     };
 
-    $scope.cancel = function() {
+    $scope.cancel = function () {
       $modalInstance.dismiss();
     };
-  })
+  });
