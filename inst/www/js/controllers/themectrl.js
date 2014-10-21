1be3841b1648edc8e9f9557f09ee5986833f983c
Bertjan Broeksema <broeksem@lippmann.lu>
2014-10-21 13:19:11 +0200

    Fix jslint issues in js/controllers/themectrl.js

--- inst/www/js/controllers/themectrl.js
+++ inst/www/js/controllers/themectrl.js
@@ -1,15 +1,18 @@
+/*jslint indent: 2 */
+/*global angular, $ */
+
 'use strict';
 
 angular.module('contigBinningApp.controllers')
-  .controller('ThemeCtrl', function($scope) {
-    
+  .controller('ThemeCtrl', function ($scope) {
+
     var link = $('#maincss');
-    
+
     $scope.theme = 'light';
-    $scope.$watch('theme', function(newTheme) {
-       var url = 'css/' + newTheme + '.css';
-       if (link.attr('href') !== url) {
-         link.attr('href', url);
-       }
+    $scope.$watch('theme', function (newTheme) {
+      var url = 'css/' + newTheme + '.css';
+      if (link.attr('href') !== url) {
+        link.attr('href', url);
+      }
     });
   });
