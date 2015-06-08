551eb603ae9387c69404ec198677193eb2565d7b
Bertjan Broeksema <bertjan.broeksema@list.lu>
2015-06-08 11:34:11 +0200

    Integrated the new dim. red. plot into RParcoords
    Please note that the dimredplot code has been added as a submodule,
    which means you need to take extra care when cloning the project (e.g.)
    use the --recursive option.

--- R/dimred.R
+++ R/dimred.R
@@ -9,51 +9,83 @@ p.dimred.methods <- function() {
   )
 }
 
-p.plotdata <- function(dim.red.res, column.results) {
-  # Now wrangle the result in a format suitable for plotting.
-  # Make sure that we don't select too much variables when #row < #vars
-  factors <- min(nrow(dim.red.res[[column.results]]$coord) - 1, length(dim.red.res$row$inertia) - 1)
-  factors <- 1:factors
-  plotdata <- data.frame(dim.red.res[[column.results]]$coord[ ,factors],
-                         dim.red.res[[column.results]]$contrib[, factors])
+p.projections <- function(dim.red.res, column.results) {
+  # marge.row and marge.col are respectively the masses for the rows and the columns
+  weightName <- "marge.row"
+  if (column.results == "var" || column.results == "col") {
+    weightName <- "marge.col"
+  }
 
-  # Set some propert column names.
-  # There are N - 1 factors, where N is the dimensionality of the original dataset
-  colNames <- c(mapply(function(x) paste("factor.", x, sep=""), factors),
-                mapply(function(x) paste("contrib.", x, sep=""), factors))
+  factors <- 1:(ncol(dim.red.res[[column.results]]$coord))
+  projections <- data.frame(matrix(NA, nrow=nrow(dim.red.res[[column.results]]$coord), ncol=4))
 
-  # If we have N coordinates, we have N-1 pairs of (consecutive) coordinates.
-  # For each pair, we perfom hierarchical clusterings, and make cuts in the
-  # resulting tree to get clusterings with 2,3,4,...,12 clusters. (So eleven
-  # clusterings for each pair).
-  #pairs <- c(1:(ncol(dim.red.res[[column.results]]$coord) - 1))
-  #maxClusterCount <- min(12, nrow(plotdata))
-  #mapply(function(pair) {
-  #  pairData <- plotdata[, c(pair, pair + 1)]
-  #  pairDistances <- dist(pairData)
-  #  clusterings <- cutree(hclust(pairDistances), k=c(2:maxClusterCount))
-  #  cnames <- mapply(function(x) paste("clustering.", pair, ".", x, sep=""), c(2:maxClusterCount))
+  projections$label <- rownames(dim.red.res[[column.results]]$coord)
+  projections$mass <- dim.red.res$call[[weightName]]
+  projections$coord <- dim.red.res[[column.results]]$coord[, factors]
+  projections$contrib <- dim.red.res[[column.results]]$contrib[, factors]
 
-    # Note: the double << are required to assign new values to variables outside
-    #       the scope of this mapply.
-  #  plotdata <<- data.frame(plotdata, clusterings)
-  #  colNames <<- c(colNames, cnames)
-  #}, pairs)
+  # We do not use the official rownames as the label tag is a bit friendlier
+  rownames(projections) <- NULL
 
-  colnames(plotdata) <- colNames;
-  plotdata$label <- rownames(plotdata)
-  rownames(plotdata) <- NULL
+  projections
+}
 
+p.plotdata <- function(dim.red.res, processedData, type) {
+  varName <- "var"
+  indName <- "ind"
+  if (type == "CA") {
+    varName <- "col"
+    indName <- "row"
+  }
+
+  # If we have a large number of individuals the JSON will be too big and the visualisation will fail.
+  # TODO: fine-tune the 1000 individuals threshold
+  individualProjections <- NULL
+  includeIndividuals <- nrow(dim.red.res[[indName]]$coord) < 1000
+  if (includeIndividuals) {
+    individualProjections <- p.projections(dim.red.res, column.results = indName)
+  }
+
+  analysis <- list(
+    variableProjections = p.projections(dim.red.res, column.results = varName),
+    individualProjections = individualProjections,
+    explainedVariance = dim.red.res$eig$"percentage of variance",
+    method = type
+  )
+
+  if (!includeIndividuals) {
+    analysis$individualProjections <- NULL
+  }
+
+  # TODO: optimise processedData storage, currently it is a data.frame,
+  #       which means that every column name is present in every row
   list(
-    projections=plotdata,
-    explainedVariance=dim.red.res$eig$"percentage of variance"[factors]
+    processedData = processedData,
+    analyses = list(analysis)
   )
 }
 
 # dimred.pca(vars = c("M4", "M20", "M28", "M36", "M40", "M44", "M48"))
 dimred.pca <- function(rows=c(), vars) {
-  data <- data.get(rows, vars, addRows = F)
-  p.plotdata(FactoMineR::PCA(data, ncp=length(vars), graph=F), column.results="var")
+  data <- data.get(rows, vars, addRows = T)
+
+  # The database calls the column with row names "row" instead of R's "row.names"
+  rownames(data) <- data$row
+  data$row <- NULL
+
+  # Create a dataframe with normalised data, to be used for the calculation of influence
+  PCAmax <- apply(abs(data), 2, max)
+  PCAmin <- apply(abs(data), 2, min)
+  PCAextent <- PCAmax - PCAmin
+  processedData <- abs(data) - t(replicate(nrow(data), PCAmin))
+  processedData <- t(diag(1/PCAextent) %*% t(processedData))
+  processedData <- as.data.frame(processedData)
+
+  colnames(processedData) <- colnames(data)
+  processedData$name <- rownames(data)
+  rownames(processedData) <- NULL
+
+  p.plotdata(FactoMineR::PCA(data, ncp=length(vars), graph=F), processedData = processedData, type = "PCA")
 }
 
 # dimred.ca(vars=c("aaaa","aaac","aaag","aaat","aaca","aacc","aacg","aact","aaga","aagc","aagg","aagt","aata","aatc","aatg","aatt","acaa","acac","acag","acat","acca","accc","accg","acct","acga","acgc","acgg","acgt","acta","actc","actg","actt","agaa","agac","agag","agat","agca","agcc","agcg","agct","agga","aggc","aggg","aggt","agta","agtc","agtg","agtt","ataa","atac","atag","atat","atca","atcc","atcg","atct","atga","atgc","atgg","atgt","atta","attc","attg","attt","caaa","caac","caag","caat","caca","cacc","cacg","cact","caga","cagc","cagg","cagt","cata","catc","catg","catt","ccaa","ccac","ccag","ccat","ccca","cccc","cccg","ccct","ccga","ccgc","ccgg","ccgt","ccta","cctc","cctg","cctt","cgaa","cgac","cgag","cgat","cgca","cgcc","cgcg","cgct","cgga","cggc","cggg","cggt","cgta","cgtc","cgtg","cgtt","ctaa","ctac","ctag","ctat","ctca","ctcc","ctcg","ctct","ctga","ctgc","ctgg","ctgt","ctta","cttc","cttg","cttt"))
@@ -62,8 +94,20 @@ dimred.ca <- function(rows=c(), vars) {
   # TODO: Instead of this matrix use the burt matrix where rows represent
   #       columns as well. It must be something ling: data x data^T. Its size
   #       is number of cols X number of cols. It should make the operation.
-  data <- data.get(rows, vars, addRows=F)
-  p.plotdata(FactoMineR::CA(data, ncp=length(vars), graph=F), column.results="col")
+  data <- data.get(rows, vars, addRows=T)
+
+  # The database calls the column with row names "row" instead of R's "row.names"
+  rownames(data) <- data$row
+  data$row <- NULL
+
+  CAres <- FactoMineR::CA(data, ncp=length(vars), graph=F)
+
+  # CAres$call$X contains the normalised data
+  processedData <- CAres$call$X
+  processedData$name <- rownames(processedData)
+  rownames(processedData) <- NULL
+
+  p.plotdata(CAres, processedData = processedData, type = "CA")
 }
 
 # dimred.summarize(variableWeights=list(aaaa=c(6.0151, 7.0562), aaat=c(4.7641, 2.7563), aata=c(4.175, 0.851), aatt=c(4.4429, 2.0970), ataa=c(4.1547, 0.8407), atat=c(3.1881, 0.1051), atta=c(4.1958, 0.7357), attt=c(4.7248, 2.7704)))
--- inst/www/js/controllers/dimredplotctrl.js
+++ inst/www/js/controllers/dimredplotctrl.js
@@ -1,150 +1,284 @@
 /*jslint browser: true, todo:true, nomen: true, indent: 2 */
-/*global angular, crpgl, d3, ocpu, _, $*/
+/*global angular, crpgl, list, d3, ocpu, _, $*/
 
 angular.module('contigBinningApp.controllers')
   .controller('DimRedPlotCtrl', function ($scope, $element, $window, ParCoords) {
-
     'use strict';
+    /*jslint unparam:true*/
 
-    var plot = crpgl.DimRedPlot(),
+    var dimredplot = [],
       data,
-      pair = 1,
-      clusterCount = 1;
+      selections = {
+        variable: {},
+        individual: {}
+      },
+      influences = {
+        variable: {},
+        individual: {}
+      };
 
-    /*jslint unparam: true*/
-    function rotate(drp, direction) {
-      var update = false;
-      if (direction === "up" && pair > 1) {
-        pair = pair - 1;
-        update = true;
-      } else if (direction === "down" && pair < data.explainedVariance.length - 1) {
-        pair = pair + 1;
-        update = true;
+    /* Functions for preparing the PCA/CA/MCA analysis data */
+    function resetStates() {
+      selections.variable = {};
+      selections.individual = {};
+      influences.variable = {};
+      influences.individual = {};
+
+      data.processedData.forEach(function (row) {
+        selections.individual[row.name] = list.selected.NONE;
+        influences.individual[row.name] = 0;
+      });
+
+      var colName;
+
+      if (data.processedData.length === 0) {
+        return;
       }
 
-      if (update) {
-        data.actives = [pair, pair + 1];
-        d3.select($element[0])
-          .datum(data)
-          .call(plot);
-      }
-    }
-    /*jslint unparam: false */
-
-    function pointClick(values) {
-      // We assume no clustering, so values, will always contain *one* variable.
-      var colName = _.keys(values)[0],
-        varIndex = _.findIndex(ParCoords.variables, { "name": colName }),
-        curVarIndex = _.findIndex(ParCoords.selectedVariables, { "name": colName }),
-        selectedVars;
-
-      if (curVarIndex === -1) {
-        selectedVars = ParCoords.selectedVariables;
-        selectedVars.push(ParCoords.variables[varIndex]);
-        ParCoords.updateSelectedVariables(selectedVars);
+      for (colName in data.processedData[0]) {
+        if (data.processedData[0].hasOwnProperty(colName)) {
+          selections.variable[colName] = list.selected.NONE;
+          influences.variable[colName] = 0;
+        }
       }
     }
 
-    plot
-      .originSize(30)
-      .originVisible(true)
-      .on("rotate", rotate)
-      .on("pointclick", pointClick);
+    // Makes sure that the labels of points are no longer than 80px
+    function createWrappedLabels(points) {
+      var testText = d3.select("body").append("div").style("float", "left");
 
-    function cluster(newClusterCount) {
-      var clusteredData = {
-          projections: data.projections,
-          actives: data.actives,
-          explainedVariance: data.explainedVariance
-        },
-        prop,
-        clustered,
-        points;
+      testText.style("font-size", "8px");
 
-      if (clusterCount === newClusterCount) {
-        return; // Nothing to do.
+      function wrap(d, i) {
+        /*jslint unparam:true*/
+        var textLength,
+          string,
+          desiredStringLength;
+
+        testText.text(d.label);
+        textLength = testText.node().offsetWidth;
+
+        string = d.label;
+        desiredStringLength = Math.ceil(80 / textLength * string.length);
+
+        string = string.slice(0, desiredStringLength);
+        testText.text(string);
+        textLength = testText.node().clientWidth;
+
+        while (textLength > 80 && string.length > 0) {
+          string = string.slice(0, -1);
+          testText.text(string);
+          textLength = testText.node().clientWidth;
+        }
+
+        d.wrappedLabel = string;
       }
 
-      clusterCount = newClusterCount;
-      if (clusterCount > 1) {
-        prop = { cluster: "clustering." + pair  + "." + clusterCount };
-        clustered = _.groupBy(data.projections, prop.cluster);
-        points = [];
+      points.forEach(wrap);
 
-        prop.factorX = "factor." + data.actives[0];
-        prop.factorY = "factor." + data.actives[1];
-        prop.contribX = "contrib." + data.actives[0];
-        prop.contribY = "contrib." + data.actives[1];
+      testText.remove();
+    }
 
-        clusteredData.actives = data.actives;
+    /* Functions for handling dimredplot events and updates */
+    function updateStates(influencedComponent, selectedComponent, variablesAreInfluenced) {
+      var max = 0,
+        dataIsSelected = false,
+        stateKey;
 
-        _.forIn(clustered, function (cluster) {
-          // cluster is an array of points.
-          // The position of the cluster is determined by taking averages for
-          // x and y positions. The size of the cluster is determined by
-          // summing contributions.
-          var clusterPoint = {};
-          clusterPoint[prop.factorX] = 0;
-          clusterPoint[prop.factorY] = 0;
-          clusterPoint[prop.contribX] = 0;
-          clusterPoint[prop.contribY] = 0;
-          clusterPoint.points = cluster;
-
-          _.forEach(cluster, function (point) {
-            clusterPoint[prop.factorX] += point[prop.factorX];
-            clusterPoint[prop.factorY] += point[prop.factorY];
-            clusterPoint[prop.contribX] += point[prop.contribX];
-            clusterPoint[prop.contribY] += point[prop.contribY];
-          });
-
-          // Average the position
-          clusterPoint[prop.factorX] /= cluster.length;
-          clusterPoint[prop.factorY] /= cluster.length;
-          points.push(clusterPoint);
+      // Points that are selected are not influenced and influenced points need their influence to be reset
+      _.forEach(influences, function (states, component) {
+        _.forEach(states, function (val, key) {
+          /*jslint unparam:true*/
+          influences[component][key] = 0;
         });
+      });
 
-        clusteredData.projections = points;
+      // If points are influenced then they are not selected
+      _.forEach(selections[influencedComponent], function (val, key) {
+        /*jslint unparam:true*/
+        selections[influencedComponent][key] = list.selected.NONE;
+      });
+
+      data.processedData.forEach(function (row) {
+        var colName,
+          influencedName,
+          selectedName;
+
+        if (variablesAreInfluenced === false || selections[selectedComponent][row.name] !== list.selected.NONE) {
+          for (colName in row) {
+            if (row.hasOwnProperty(colName)) {
+              if (variablesAreInfluenced) {
+                influencedName = colName;
+                selectedName = row.name;
+              } else {
+                influencedName = row.name;
+                selectedName = colName;
+              }
+
+              if (selections[selectedComponent][selectedName] !== list.selected.NONE && colName !== "name") {
+                dataIsSelected = true;
+                influences[influencedComponent][influencedName] += row[colName];
+                max = Math.max(max, influences[influencedComponent][influencedName]);
+              }
+            }
+          }
+        }
+      });
+
+      if (!dataIsSelected) {
+        return;
       }
 
-      d3.select($element[0])
-        .datum(clusteredData)
-        .call(plot);
+      // Influence lies between 0 and 1
+      for (stateKey in influences[influencedComponent]) {
+        if (influences[influencedComponent].hasOwnProperty(stateKey)) {
+          influences[influencedComponent][stateKey] /= max;
+        }
+      }
     }
+
+    function changeSelection(drp) {
+      /*jslint unparam:true*/
+      data.flags.pointsChanged = false;
+
+      d3.select($element[0]).selectAll("div.dimredplot")
+        .data(data.analyses)
+        .each(function (d, i) {
+          /*jslint unparam:true*/
+          dimredplot[i](d3.select(this));
+        });
+    }
+
+    function changeVariableSelection(drp) {
+      var buttonDisabled = "disabled",
+        stateKey;
+
+      selections.variable = drp.variableSelections();
+
+      for (stateKey in selections.variable) {
+        if (selections.variable.hasOwnProperty(stateKey) && selections.variable[stateKey] !== list.selected.NONE) {
+          buttonDisabled = null;
+          break;
+        }
+      }
+
+      d3.select("button#changeParcoordsVariables")
+        .attr("disabled", buttonDisabled);
+
+      updateStates("individual", "variable", false);
+
+      dimredplot.forEach(function (d) {
+        if (d !== drp) {
+          d.variableSelections(selections.variable);
+        }
+        d.individualInfluences(influences.individual);
+      });
+
+      changeSelection(drp);
+    }
+
+    function changeIndividualSelection(drp) {
+      selections.individual = drp.individualSelections();
+      updateStates("variable", "individual", true);
+
+      dimredplot.forEach(function (d) {
+        if (d !== drp) {
+          d.individualSelections(selections.individual);
+        }
+        d.variableInfluences(influences.variable);
+      });
+
+      changeSelection(drp);
+    }
+
+    d3.select("button#changeParcoordsVariables")
+      .on("click", function () {
+        var selectedVariables = {},
+          stateKey;
+
+        for (stateKey in selections.variable) {
+          if (selections.variable.hasOwnProperty(stateKey) && selections.variable[stateKey] !== list.selected.NONE) {
+            selectedVariables[stateKey] = selections.variable[stateKey];
+          }
+        }
+
+        //setDimensions(selectedVariables);
+      });
+
 
     function resize() {
-      plot
-        .width($element[0].clientWidth)
-        .height($element[0].clientHeight);
-      d3.select($element[0]).call(plot);
+      d3.select($element[0]).selectAll("div.dimredplot")
+        .style("width", $element[0].clientWidth / dimredplot.length)
+        .style("height", $element[0].clientHeight)
+        .each(function (d, i) {
+          /*jslint unparam:true*/
+          dimredplot[i]
+            .width($element[0].clientWidth / dimredplot.length)
+            .height($element[0].clientHeight);
+          dimredplot[i](d3.select(this));
+        });
     }
 
     function updatePlot(dimRedData) {
-      pair = 1; // Always start with the first pair for a new plot
-      data = dimRedData;
-      data.actives = [pair, pair + 1];
-      // TODO: Introduce various ways to 'cut' the number of principal axes.
-      //       Various approaches could be taken, such as taking the axes upto
-      //       a certain amount of explained variances or retaint only the
-      //       first N principal axes. In both cases the variance and N should
-      //       be configurable by the user.
-      //data.explainedVariance = data.explainedVariance.slice(0, 10);
+      var div;
 
-      d3.select($element[0])
-        .datum(data)
-        .call(plot);
+      dimredplot = [];
+      data = dimRedData;
+      data.flags = {pointsChanged: true};
+      resetStates();
+
+      data.analyses.forEach(function (analysis) {
+        if (analysis.variableProjections !== undefined) {
+          createWrappedLabels(analysis.variableProjections);
+        }
+        if (analysis.individualProjections !== undefined) {
+          if (analysis.individualProjections.length > 1000) {
+            analysis.individualProjections = undefined;
+          } else {
+            createWrappedLabels(analysis.individualProjections);
+          }
+        }
+      });
+
+      data.analyses.forEach(function () {
+        var chart = list.DimRedPlot();
+        chart
+          .originSize(30)
+          .originVisible(true)
+          .on("changeVariableSelection", changeVariableSelection)
+          .on("changeIndividualSelection", changeIndividualSelection)
+          .individualSelections(selections.individual)
+          .variableSelections(selections.variable)
+          .individualInfluences(influences.individual)
+          .variableInfluences(influences.variable);
+        dimredplot.push(chart);
+      });
+
+      div = d3.select($element[0]).selectAll("div.dimredplot").data(data.analyses);
+      div.enter()
+        .append("div")
+        .attr("class", "dimredplot");
+
+      div
+        .style("width", $element[0].clientWidth / data.analyses.length)
+        .style("height", $element[0].clientHeight)
+        .each(function (d, i) {
+          /*jslint unparam:true*/
+          dimredplot[i]
+            .width($element[0].clientWidth / data.analyses.length)
+            .height($element[0].clientHeight);
+          dimredplot[i](d3.select(this));
+        });
+
+      div.exit().remove();
     }
 
     angular.element($window).bind('resize', resize);
     $(document).ready(resize);
 
-    /*jslint unparam: true */
+
     $scope.$on("Analytics::dimensionalityReduced", function (ev, method, session) {
+      /*jslint unparam: true */
       session.getObject(updatePlot);
     });
-    /*jslint unparam: false */
-    /*jslint unparam: true */
-    $scope.$on("DimRedDisplay::cluster", function (ev, clusterCount) {
-      cluster(clusterCount);
-    });
-    /*jslint unparam: false */
   });
