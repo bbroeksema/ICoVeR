ae51b1572f53b95ebf291941f7fcde8e6bd080c3
Bertjan Broeksema <bertjan.broeksema@list.lu>
2015-11-03 11:01:42 +0100

    Add code for preparing wrighton

--- R.preprocessing/preprocessing.R
+++ R.preprocessing/preprocessing.R
@@ -5,6 +5,7 @@ source("R.preprocessing/SymmetrizedSignatures.R")
 source("R.preprocessing/ExtractESCG.R")
 source("R.preprocessing/PrepareDataForInteractiveBinning.R")
 
+# Prepare the CSTR data set
 PrepareDataForInteractiveBinning(
  dataset.name = "cstr",
  file.fasta  = "data//cstr_assembled.fasta",
@@ -12,6 +13,16 @@ PrepareDataForInteractiveBinning(
  file.escg = "data/cstr_escg.csv",
  file.clusterings = "data//cstr_clusterings.csv",
  dir.result = "R.ICoBiRe//data"
+)
+
+# Prepare the Wrighton data set
+PrepareDataForInteractiveBinning(
+  dataset.name = "wrighton",
+  file.fasta  = "data//wrighton_assembled.fasta",
+  file.abundance = "data//wrighton_avg_coverage.csv",
+  file.escg = "data//wrighton_escg.csv",
+  file.clusterings = "data//wrighton_clusterings.csv",
+  dir.result = "R.ICoBiRe//data"
 )
 
 #library(devtools)
