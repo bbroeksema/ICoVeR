c2ec60ce0f1a29f070df26f49df2bd8416c01101
Bertjan Broeksema <bertjan.broeksema@list.lu>
2015-11-04 15:47:09 +0100

    Remove unused file for exporting contigs.

--- R.ICoVeR/R/contigs.R
+++
@@ -1,7 +0,0 @@
-contigs.export <- function(rows = c()) {
-  contigIds <- data.get(rows, c("CONTIG"), addRows=F)
-  data(list=list(p.db.dataset))
-  fasta <- .GlobalEnv[[paste(p.db.dataset, ".fasta", sep="")]]
-  fasta <- fasta[names(fasta) %in% contigIds]
-  Biostrings::writeXStringSet(fasta, "x.fasta")
-}
