3e29e97d92cad9be873b0ac928a426f38b11573f
Bertjan Broeksema <dev@broeksemaatjes.nl>
2015-11-13 22:57:01 +0100

    Allign preparation for wrighton
    We have now all the files to prepare the wrighton dataset, as well as the
    script to perform the preparation and to start the application.
    
    This should users give a good deal of information to prepare their own
    datasets.

--- R.preprocessing/preprocessing.R
+++ R.preprocessing/preprocessing.R
@@ -1,29 +1,32 @@
-#setwd("~/icobire-guide")
+# Adapt this path to the location in which you cloned the ICoVeR project from
+# github.
+#setwd("~/ICoVeR")
 
 source("R.preprocessing/FrequenciesSignatures.R")
 source("R.preprocessing/SymmetrizedSignatures.R")
 source("R.preprocessing/ExtractESCG.R")
 source("R.preprocessing/PrepareDataForInteractiveBinning.R")
 
-# Prepare the CSTR data set
-PrepareDataForInteractiveBinning(
- dataset.name = "cstr",
- file.fasta  = "data//cstr_assembled.fasta",
- file.abundance = "data//cstr_avg_coverage.csv",
- file.escg = "data/cstr_escg.csv",
- file.clusterings = "data//cstr_clusterings.csv",
- dir.result = "R.ICoBiRe//data"
-)
-
 # Prepare the Wrighton data set
 PrepareDataForInteractiveBinning(
   dataset.name = "wrighton",
-  file.fasta  = "data//wrighton_assembled.fasta",
-  file.abundance = "data//wrighton_avg_coverage.csv",
+  file.fasta  = "data//wrighton_assembly.fasta.gz",
+  file.abundance = "data//wrighton_avg_cov.csv",
   file.escg = "data//wrighton_escg.csv",
   file.clusterings = "data//wrighton_clusterings.csv",
-  dir.result = "R.ICoBiRe//data"
+  dir.result = "R.ICoVeR//data"
 )
 
-#library(devtools)
-#install_local(file.path(getwd(), "R.ICoBiRe"))
+# Install the ICoVeR package after data generation.
+# NOTE: Before running install_local, make sure that R.ICoVeR/R/sqlite.R
+#       is configured properly. The variable p.db.dataset must be assigned the
+#       same value as the value passed to dataset.name in
+#       PrepareForInteractiveBinning.
+library(devtools)
+install_local(file.path(getwd(), "R.ICoVeR"))
+
+# Start opencpu and launch ICoVeR in the browser
+library(opencpu)
+opencpu$stop() # It starts at a random port, which is annoying.
+opencpu$start(8000)
+browseURL("http://localhost:8000/ocpu/library/ICoVeR/www/", browser = getOption("browser"), encodeIfNeeded = FALSE)
